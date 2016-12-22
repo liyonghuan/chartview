ac39de21d5525731cbd01736e383c2c4c3a5df02
Klavor Lee <webmaster@klavor.com>
2016-12-22 14:50:21 +0800

    Add a horizontal bar chart

--- Android/app/src/main/java/com/geyek/widget/MainActivity.java
+++ Android/app/src/main/java/com/geyek/widget/MainActivity.java
@@ -20,29 +20,18 @@ public class MainActivity extends AppCompatActivity {
         demoBackground.setSubLineColor(Color.parseColor("#1F1F1F"));
         demoBackground.setLineWidth(5);
         demoBackground.setSubLineWidth(2);
-        demoBackground.setXNum(3);
-        demoBackground.setSubXNum(5);
-        demoBackground.setYNum(3);
-        demoBackground.setSubYNum(10);
+        demoBackground.setXNum(2);
+        demoBackground.setSubXNum(10);
+        demoBackground.setYNum(1);
+        demoBackground.setSubYNum(0);
         mCharView.setForefground(demoBackground);
 
         DemoBarChart demoBarChart = new DemoBarChart();
-        demoBarChart.setMaxXValue(15);
-        demoBarChart.setMaxYValue(30);
+        demoBarChart.setMaxXValue(60);
+        demoBarChart.setMaxYValue(1);
         demoBarChart.setShaderColor(Color.RED, Color.YELLOW);
-        demoBarChart.setData(10);
-        demoBarChart.setData(20);
-        demoBarChart.setData(30);
-        demoBarChart.setData(15);
-        demoBarChart.setData(3);
-        demoBarChart.setData(24);
-        demoBarChart.setData(13);
-        demoBarChart.setData(22);
-        demoBarChart.setData(7);
-        demoBarChart.setData(15);
-        demoBarChart.setData(28);
-        demoBarChart.setData(14);
-        demoBarChart.setData(5, 2);
+        demoBarChart.setData(54);
+        demoBarChart.setFangXiang(DemoBarChart.FangXiang.HORIZONTAL);
         mCharView.setChart(demoBarChart);
     }
 }
