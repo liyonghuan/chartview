25daa7046d1d23f23221ea8ac9a82766a74d5c54
Klavor Lee <lee@klavor.com>
2016-12-30 00:57:26 +0800

    Optimize CurveChart Code

--- Android/app/src/main/java/com/geyek/widget/diagram/chart/CurveChart.java
+++ Android/app/src/main/java/com/geyek/widget/diagram/chart/CurveChart.java
@@ -11,6 +11,7 @@ import com.geyek.widget.diagram.GeyekChartView;
  */
 public class CurveChart extends LineChart {
     private static final String TAG = "CurveChart";
+    private Path mPath = new Path();
 
     public CurveChart(GeyekChartView chartView) {
         super(chartView);
@@ -46,17 +47,18 @@ public class CurveChart extends LineChart {
             float endX = offset + i * cellWidth;
             float endY = drawableHeight - (currentPoint - mMinValue) * cellHeight;
 
-            Path path2 = new Path();
             //设置Path的起点
-            path2.moveTo(startX,startY);
+            mPath.moveTo(startX, startY);
 
             float tempx = (startX + endX) / 2;
 
             //设置贝塞尔曲线的控制点坐标和终点坐标
-            path2.cubicTo(tempx, startY, tempx, endY, endX, endY);
+            mPath.cubicTo(tempx, startY, tempx, endY, endX, endY);
 
             //画出贝塞尔曲线
-            canvas.drawPath(path2, paint);
+            canvas.drawPath(mPath, paint);
+
+            mPath.reset();
 
             prePoint = currentPoint;
         }
